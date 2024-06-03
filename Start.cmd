07e6ba4bb5a756b5ffd736a15c7089182e329649
LouisVraie <louis.vraie@gmail.com>
2024-06-03 10:03:19 +0200

    feat: added docker rm to Start.cmd before run

--- Start.cmd
+++ Start.cmd
@@ -2,8 +2,8 @@ docker build --pull --rm -f "./Dockerfile" -t gestiondeprojet-loginapp:latest ".
 
 docker network create -d bridge lb-net
 
-docker run -d -p 8081:8081 --net=lb-net --name registery gestiondeprojet-loginapp
+docker rm -f registery && docker run -d -p 8081:8081 --net=lb-net --name registery --rm gestiondeprojet-loginapp
 
-docker run -d --net=lb-net --name worker1 -e HOSTNAME=worker1 gestiondeprojet-loginapp
-docker run -d --net=lb-net --name worker2 -e HOSTNAME=worker2 gestiondeprojet-loginapp
-docker run -d --net=lb-net --name worker3 -e HOSTNAME=worker3 gestiondeprojet-loginapp
+docker rm -f worker1 && docker run -d --net=lb-net --name worker1 -e HOSTNAME=worker1 gestiondeprojet-loginapp
+docker rm -f worker2 && docker run -d --net=lb-net --name worker2 -e HOSTNAME=worker2 gestiondeprojet-loginapp
+docker rm -f worker3 && docker run -d --net=lb-net --name worker3 -e HOSTNAME=worker3 gestiondeprojet-loginapp
